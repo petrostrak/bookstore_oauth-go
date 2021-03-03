6ce86fc6cb9eb23228cd15ac3fd4ce933056ad6c
petros <pit.trak@gmail.com>
2021-03-03 14:45:51 +0200

    minor fix in AuthenticateRequest

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -77,6 +77,9 @@ func AuthenticateRequest(req *http.Request) *errors.RestErr {
 
 	at, err := getAccessToken(accessToken)
 	if err != nil {
+		if err.Status == http.StatusNotFound {
+			return nil
+		}
 		return err
 	}
 
